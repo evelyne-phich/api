c2f378a311753c93867bfc392c79d1dad0c5ca29
evelyne-phich <evelyne.phich@gmail.com>
2024-08-16 15:49:43 +0200

    handle cors

--- app.ts
+++ app.ts
@@ -3,9 +3,12 @@ dotenv.config();
 
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(router);
 
