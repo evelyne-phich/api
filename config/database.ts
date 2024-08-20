5d8f8e39620cf00eb0ac4daeb099ad167f6b356c
evelyne-phich <evelyne.phich@gmail.com>
2024-08-20 21:16:21 +0200

    update database config to connection string

--- config/database.ts
+++ config/database.ts
@@ -1,28 +1,6 @@
 import { Client } from "pg";
 import { env } from "./env";
 
-type DatabaseConfig = {
-  host?: string;
-  port?: number;
-  user?: string;
-  password?: string;
-  database?: string;
-  ssl: {
-    rejectUnauthorized: boolean;
-  };
-};
-
-const databaseConfig: DatabaseConfig = {
-  host: env.PG_HOST,
-  port: env.PG_PORT,
-  user: env.PG_USER,
-  password: env.PG_PASSWORD,
-  database: env.PG_DATABASE,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
-
-export const client = new Client(databaseConfig);
+export const client = new Client(env.PG_CONNECTION_STRING);
 
 client.connect();
--- config/env.ts
+++ config/env.ts
@@ -1,8 +1,4 @@
 export const env = {
-  PG_HOST: process.env.PG_HOST,
-  PG_PORT: Number(process.env.PG_PORT),
-  PG_USER: process.env.PG_USER,
-  PG_PASSWORD: process.env.PG_PASSWORD,
-  PG_DATABASE: process.env.PG_DATABASE,
   NODE_ENV: process.env.NODE_ENV,
+  PG_CONNECTION_STRING: process.env.PG_CONNECTION_STRING,
 };
