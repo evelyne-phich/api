74f22355fa928260ad696aceca31eb2936379d50
evelyne-phich <evelyne.phich@gmail.com>
2024-08-20 20:53:15 +0200

    fix database ssl config in prod

--- config/database.ts
+++ config/database.ts
@@ -1,22 +1,28 @@
 import { Client } from "pg";
 import { env } from "./env";
 
-type DevConfig = {
+type DatabaseConfig = {
   host?: string;
   port?: number;
   user?: string;
   password?: string;
   database?: string;
+  ssl: {
+    rejectUnauthorized: boolean;
+  };
 };
 
-const devConfig: DevConfig = {
+const databaseConfig: DatabaseConfig = {
   host: env.PG_HOST,
   port: env.PG_PORT,
   user: env.PG_USER,
   password: env.PG_PASSWORD,
   database: env.PG_DATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
-export const client = new Client(devConfig);
+export const client = new Client(databaseConfig);
 
 client.connect();
